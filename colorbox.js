fc6b88a99643b35d085a291873f597931e50be70
Richard-Howard-Toronto <rh_gk@rogers.com>
2018-11-04 07:43:38 -0500

    commits

---
+++ colorbox.js
@@ -0,0 +1,11 @@
+$(".a").css("background-color","#f2f6ff");
+$(".b").css("background-color","#c7bbfd");
+$(".c").css("background-color","#be86fa");
+$(".d").css("background-color","#b957f5");
+$(".e").css("background-color","#b130ed");
+$(".f").css("background-color","#a713e4");
+$(".g").css("background-color","#9b00d7");
+$(".h").css("background-color","#8300b3");
+$(".i").css("background-color","#74009e");
+
+$(".a,.b,.c,.d,.e,.f,.g,.h,.i").css.fadein(1400)
